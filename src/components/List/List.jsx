b6705b1f800d9fe20485760269915f8790ec21a4
LOCO185 <jloco151@gmail.com>
2022-07-20 14:39:46 +0300

    Adding pure React slider

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,35 +1,60 @@
 import "./List.scss";
 import ListItem from "../ListItem/ListItem";
+import { useRef, useState } from "react";
 
-import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined';
-import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
+import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
 
 const List = () => {
-  return <div className="list">
-    <span className="listTitle">Continue to watch</span>
-    <div className="wrapper">
-        <ArrowBackIosOutlinedIcon className="sliderArrow left"/>
-        <div className="container">
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 58;
+
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${355 + distance}px)`;
+    }
+
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-355 + distance}px)`;
+    }
+
+  };
+  return (
+    <div className="list">
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlinedIcon
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{display: !isMoved && "none"}}
+        />
+        <div className="container" ref={listRef}>
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
         </div>
-        <ArrowForwardIosOutlinedIcon className="sliderArrow right"/>
+        <ArrowForwardIosOutlinedIcon
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
+      </div>
     </div>
-    </div>;
+  );
 };
 
 export default List;
