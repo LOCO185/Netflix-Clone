db751028f4b76e9cd969848071f1f6bdfbaddd38
LOCO185 <jloco151@gmail.com>
2022-07-23 20:07:20 +0300

    commit

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -6,7 +6,7 @@ import AddIcon from "@mui/icons-material/Add";
 import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
 import ThumbDownOutlinedIcon from "@mui/icons-material/ThumbDownOutlined";
 
-const ListItem = ({index}) => {
+const ListItem = ({ index }) => {
   const [isHovered, setIsHovered] = useState(false);
   return (
     <div
@@ -19,24 +19,29 @@ const ListItem = ({index}) => {
         src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
         alt=""
       />
-      <div className="itemInfo">
-        <div className="icons">
-          <PlayArrowIcon />
-          <AddIcon />
-          <ThumbUpOutlinedIcon />
-          <ThumbDownOutlinedIcon />
-          <div className="itemInfoTop">
-            <span>1 hour 14 mins</span>
-            <span className="limit">+16</span>
-            <span>1999</span>
+      {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop />
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrowIcon className="icon" />
+              <AddIcon className="icon" />
+              <ThumbUpOutlinedIcon className="icon" />
+              <ThumbDownOutlinedIcon className="icon" />
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 14 mins</span>
+              <span className="limit">+16</span>
+              <span>1999</span>
+            </div>
+            <div className="desc">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Praesentium hic rem eveniet error possimus, neque ex doloribus.
+            </div>
+            <div className="genre">Action</div>
           </div>
-          <div className="desc">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Praesentium hic rem eveniet error possimus, neque ex doloribus.
-          </div>
-          <div className="genre">Action</div>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
