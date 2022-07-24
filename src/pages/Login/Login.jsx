84262a2b5420db18c3b598ac3cc3c12154f72475
LOCO185 <jloco151@gmail.com>
2022-07-24 14:42:40 +0300

    Updates on login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -12,6 +12,14 @@ const Login = () => {
           />
         </div>
       </div>
+      <div className="container">
+        <form>
+          <h1>Sign In</h1>
+          <input type="email" placeholder="Email or phone number" />
+          <input type="password" placeholder="Password" />
+          <button className="loginButton">Sign In</button>
+        </form>
+      </div>
     </div>
   );
 };
