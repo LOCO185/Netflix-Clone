58b9878aa0328c3706bea10d76e7a000f5338263
LOCO185 <jloco151@gmail.com>
2022-07-24 14:40:47 +0300

    Updates on Login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,19 @@
 import "./Login.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
