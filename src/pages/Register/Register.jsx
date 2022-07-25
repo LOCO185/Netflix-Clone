3c4a9f3c8a3ef58f381c7aec2ba5945bae58a323
LOCO185 <jloco151@gmail.com>
2022-07-25 23:40:29 +0300

    commit

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -4,7 +4,22 @@ import "./Register.scss";
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  return <div>Register</div>;
+  return (
+    <div>
+      <div className="register">
+        <div className="top">
+          <div className="wrapper">
+            <img
+              className="logo"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+              alt=""
+            />
+            <button className="loginButton">Sign In</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
