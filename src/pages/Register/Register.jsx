024c8a44efb2264984105bfb01eb178cad0a8ce2
LOCO185 <jloco151@gmail.com>
2022-07-25 23:48:03 +0300

    Final

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -4,6 +4,7 @@ import "./Register.scss";
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
   return (
     <div className="register">
       <div className="top">
