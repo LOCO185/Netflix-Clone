155d87d736a7ed680e769699262c65a1db42a827
LOCO185 <jloco151@gmail.com>
2022-07-26 16:31:05 +0300

    commit

--- src/pages/Watch/Watch.jsx
+++ src/pages/Watch/Watch.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
 
 const Watch = () => {
   return (
-    <div>Watch</div>
-  )
-}
+    <div className="watch">
+      <div className="back">
+        <ArrowBackIosNewOutlinedIcon />
+        Home
+      </div>
+    </div>
+  );
+};
 
-export default Watch
+export default Watch;
