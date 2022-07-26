5446e72e338330b78c7febbfc28cbfd06a707f51
LOCO185 <jloco151@gmail.com>
2022-07-26 16:45:33 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import "./App.scss";
 import Home from "./pages/Home/Home";
+import Register from "./pages/Register/Register";
+import Watch from "./pages/Watch/Watch";
+import Login from "./pages/Login/Login";
 
 function App() {
   return <Home />;
