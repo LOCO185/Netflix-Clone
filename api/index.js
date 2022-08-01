6b23a080775c2055f34abf53d7bfba4f9e43366d
LOCO185 <jloco151@gmail.com>
2022-08-01 19:45:27 +0300

    Added mongoose connect to index.js

--- api/index.js
+++ api/index.js
@@ -5,6 +5,14 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connection Successfull !"))
+  .catch((error) => console.log(error));
+
 app.listen(8800, () => {
   console.log("Backend server is running !");
 });
