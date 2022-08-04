f4066885158695d24534578beea2f5a8f9a3b764
LOCO185 <jloco151@gmail.com>
2022-08-04 23:35:25 +0300

    Added CRAETE method into Movies

--- api/routes/Movies.js
+++ api/routes/Movies.js
@@ -2,7 +2,19 @@ const router = require("express").Router();
 const Movie = require("../models/Movie");
 
 // CREATE
-
+router.post("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    const newMovie = new Movie(req.body);
+    try {
+      const savedMovie = await newMovie.save();
+      res.status(201).json(savedMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
 // UPDATE
 
 // DELETE
