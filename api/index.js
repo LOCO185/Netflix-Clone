b23e4eb0ecc6a20042b688cfc4295c3ecee8cdad
LOCO185 <jloco151@gmail.com>
2022-08-10 22:19:38 +0300

    Added Movies route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/Auth");
 const userRoute = require("./routes/Users");
+const movieRoute = require("./routes/Movies");
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running !");
