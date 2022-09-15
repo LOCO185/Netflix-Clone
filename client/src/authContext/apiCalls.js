cdece045b81b310bdc1cf9de32cbed15648ccb1d
LOCO185 <jloco151@gmail.com>
2022-09-15 20:39:14 +0300

    Api calls for login

--- client/src/authContext/apiCalls.js
+++ client/src/authContext/apiCalls.js
@@ -1,1 +1,14 @@
 import axios from "axios";
+import { loginFailure } from "./AuthActions";
+import { loginStart } from "./AuthActions";
+import { loginSuccess } from "./AuthActions";
+
+export const login = async (user, dispatch) => {
+  dispatch(loginStart());
+  try {
+    const res = await axios.post("auth/login", user);
+    dispatch(loginSuccess(res.data));
+  } catch (err) {
+    dispatch(loginFailure());
+  }
+};
