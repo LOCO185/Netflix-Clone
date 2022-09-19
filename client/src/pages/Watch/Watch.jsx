5a91bd973b9dd055a1c46066511709eca8e9d021
LOCO185 <jloco151@gmail.com>
2022-09-19 23:32:35 +0300

    commit

--- client/src/pages/Watch/Watch.jsx
+++ client/src/pages/Watch/Watch.jsx
@@ -1,19 +1,19 @@
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { Link, useLocation } from "react-router-dom";
+// import { Link, useLocation } from "react-router-dom";
 import "./Watch.scss";
 
 export default function Watch() {
-  const location = useLocation();
-  const movie = location.movie;
+  // const location = useLocation();
+  // const movie = location.movie;
   return (
     <div className="watch">
-      <Link to="/">
+      {/* <Link to="/"> */}
         <div className="back">
           <ArrowBackIcon />
           Home
         </div>
-      </Link>
-      <video className="video" autoPlay progress controls src={movie.video} />
+      {/* </Link> */}
+      {/* <video className="video" autoPlay progress controls src={movie.video} /> */}
     </div>
   );
 }
